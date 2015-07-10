e4110d771dbadd85249722d785bce8355c9dfdf6
App Academy Student <student@appacademy.io>
2015-07-10 15:17:29 -0700

    Start Associatable

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -20,13 +20,17 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.camelcase.singularize
   end
 end
 
