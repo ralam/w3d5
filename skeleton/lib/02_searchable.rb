b9a3661eeda94be47ef27e2bcaf28b6aff491249
App Academy Student <student@appacademy.io>
2015-07-10 14:55:01 -0700

    Add searchable module (Phase 2)

--- skeleton/lib/02_searchable.rb
+++ skeleton/lib/02_searchable.rb
@@ -1,12 +1,23 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    obj = DBConnection.execute(<<-SQL, params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(obj)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
