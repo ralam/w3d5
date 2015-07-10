82186d4e9b3fee45f697df4e179fe0220a4a7142
App Academy Student <student@appacademy.io>
2015-07-10 11:42:44 -0700

    Start through Phase1c

---
+++ skeleton/lib/01_sql_object.rb
@@ -0,0 +1,72 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+require 'byebug'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+    columns.first.map { |col| col.to_sym }
+  end
+
+  def self.finalize!
+    columns.each do |col|
+      define_method(col) do
+        attributes[col.to_sym]
+      end
+      define_method("#{col}=") do |value|
+        attributes[col.to_sym] = value
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name || self.name.tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
