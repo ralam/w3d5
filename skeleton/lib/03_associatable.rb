24c86e245f3455c70678f544fa6b739342f75600
App Academy Student <student@appacademy.io>
2015-07-10 18:13:35 -0700

    Add #belong_to, start on #has_many

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -3,6 +3,7 @@ require 'active_support/inflector'
 
 # Phase IIIa
 class AssocOptions
+
   attr_accessor(
     :foreign_key,
     :class_name,
@@ -10,11 +11,11 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
@@ -22,26 +23,38 @@ class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
     @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] || name.camelcase
+    @class_name = options[:class_name] || name.to_s.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    @foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    @foreign_key = options[:foreign_key] || "#{self_class_name.to_s.downcase}_id".to_sym
     @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] || name.camelcase.singularize
+    @class_name = options[:class_name] || name.to_s.camelcase.singularize
   end
 end
 
 module Associatable
   # Phase IIIb
+
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      id = self.send(options.foreign_key)
+      mclass = options.model_class
+      target = mclass.where(id: id).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, options)
+    define_method(name) do
+      id = self.send(options.primary_key)
+      mclass = options.model_class
+      target = mclass.where(id: id)
+      byebug
+    end
   end
 
   def assoc_options
@@ -50,5 +63,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
